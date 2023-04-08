314ccb40f5dfedfd1cfe90c4538a95babc0c46f8
carissa-chandra <carissa.tjan03@gmail.com>
2023-04-08 11:49:59 +0700

    news crud test1

---
+++ UAS_Web/add_news.php
@@ -0,0 +1,119 @@
+<?php
+  include("config.php");
+  if (isset($_POST['adddnews'])) {
+    $title=$_POST['title'];
+    $content=$_POST['content'];
+    $extension_allow	= array('png','jpg', 'jpeg');
+    $gb = $_FILES['file']['name'];
+    $x = explode('.', $gb);
+		$extension = strtolower(end($x));
+		$size = $_FILES['file']['size'];
+		$file_tmp = $_FILES['file']['tmp_name'];
+    if(in_array($extension, $extension_allow) === true){
+				if($size < 1044070){
+					move_uploaded_file($file_tmp, './img_news/'.$gb);
+                    $sql = mysqli_query($mysqli, "INSERT INTO news (title, content, date_news, img_news) VALUES ('$title', '$content', now(), '$gb')");
+					// $query = mysql_query("INSERT INTO upload VALUES(NULL, '$nama')");
+					if($sql){
+						echo 'FILE BERHASIL DI UPLOAD';
+					}else{
+						echo 'GAGAL MENGUPLOAD GAMBAR';
+					}
+				}else{
+					echo 'Size FILE TERLALU BESAR';
+				}
+			}else{
+				echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
+			}
+		}
+    // $perintah = mysqli_query($mysqli, "INSERT INTO tbl_berita (judul,isi,tanggal) VALUES ('$judul', '$isi' ,now())");
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- CSS Bootstrap -->
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
+  <!-- JS Bootstrap -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
+  <link rel="stylesheet" href="profile_edit.css" />
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/navbars/">
+  <link href="bootstrap/bootstrap-5.2.3-examples/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Add News</title>
+  <link rel="icon" type="image/png" href="img/logo-bg.png">
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="border-bottom: 2px solid #7C924E; background-color: white;">
+      <div class="container-fluid">
+        <a href="#"><img src="img/LOGO.png" style="width: 100px;"></a>
+        <button class="navbar-toggler btn-outline-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #7C924E">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ms-auto my-2 my-lg-0" style="font-size: 16px; margin-left: 6px; color: black;">
+            <li class="nav-item">
+              <a class="nav-link active"  aria-current="page" href="home.php"><span style="color: #198754;">Home</span></a>
+            </li>
+                    
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="vertical-align: middle; color: black;">All</a>
+
+              <ul class="dropdown-menu dropdwown-menu-light" aria-labelledby="navbarDropdownNews">
+                <li><a class="dropdown-item" href="debut_comeback.php" style="color: black;">Debut/Comeback</a></li>
+                <li><a class="dropdown-item" href="k-drama.php" style="color: black;">K-Drama</a></li>
+                <li><a class="dropdown-item" href="hot_scandals.php" style="color: black;">Hot Scandals</a></li>
+              </ul>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" href="about_us.php" style="color: black;">About Us</a>
+            </li>
+                    
+            <li class="nav-item">
+              <a class="nav-link" href="profile.php" style="color: black;">Profile</a>
+            </li>
+                    
+            <li><hr class="dropdown-divider"></li>
+                    
+            <li class="nav-item pointer">        
+              <button class="nav-link" onclick="myFunction()" style="color: black; border-style: none; background-color: white;">Log Out</button>
+              <script>
+                function myFunction() {
+                  if (confirm("Are you sure to log out?")) {
+                    window.location.href='log_out.php';
+                  } else {
+                    window.location.href='';
+                  }
+                }
+              </script>
+            </li>        
+          </ul>
+        </div>
+      </div>
+    </nav>
+<div class="container" style="margin-top: 80px;">
+    <h3 class="pb-2 border-bottom">Tambah Berita</h3>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div style="padding: 0 50px;">
+            <div class="col-12">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="" fvalue="<?php echo $row['title'] ?>" dprocessedid="l7js95">
+            </div>
+            <div class="col-12" style="margin-top: 10px;">
+                <label for="content" class="form-label">Content</label>
+                <textarea class="form-control" id="content" name="content" placeholder="" fvalue="<?php echo $row['content'] ?>" dprocessedid="l7js95"></textarea>
+            </div>
+            <div class="col-12" style="margin-top: 10px;">
+                <label for="img_news" class="form-label">News Image</label>
+                <input type="file" class="form-control" id="img_news" name="file" placeholder="" fvalue="<?php echo $row['img_news'] ?>" dprocessedid="l7js95">
+            </div>
+            <input class="btn btn-success" style="float: right; margin-top: 20px;" type="submit" name="addnews" value="tambah berita">
+        </div>
+    </form>
+</div>
+</body>
--- UAS_Web/home.php
+++ UAS_Web/home.php
@@ -80,9 +80,32 @@
     <section class="News">
       <div class="container" id="product-cards">
         <div class="container px-4 py-5" id="custom-cards">
-          <h2 class="pb-2 border-bottom" style="font-weight: bold;">Today's News</h2>
-      
+          <div class="pb-2 border-bottom">
+            <h2 class="pb-2" style="font-weight: bold; display: inline;">Today's News</h2>
+            <a href="add_news.php" style="display: inline; margin-left: 30px;">ADD</a>
+            </div>
           <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+            <!-- tambah news db php -->
+            <div class="col">
+                <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/kard-home.png');">
+                  <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                      <a href="" style="text-decoration: none; color: white">KARD is preparing comeback this April</a>
+                    </h3>
+                    <ul class="d-flex list-unstyled mt-auto">
+                      <li class="me-auto">
+                        <img src="img/logo-bg.png" alt="O-KI" width="32" height="32" class="rounded-circle border border-white">
+                      </li>
+                      <li class="d-flex align-items-center">
+                        <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                        <small>2 minutes ago</small>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+            <!-- end tambah news db php -->
+            
             <div class="col">
               <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/kard-home.png');">
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
