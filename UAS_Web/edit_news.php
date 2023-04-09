aa6eafbd01a4bd60f344ca0c6b8352ca660352ec
carissa-chandra <carissa.tjan03@gmail.com>
2023-04-09 12:43:49 +0700

    edit news process

--- UAS_Web/edit_news.php
+++ UAS_Web/edit_news.php
@@ -95,7 +95,34 @@
                     <label for="news_image" class="form-label">News Image</label>
                     <input type="file" class="form-control" id="news_image" name="news_image" placeholder="" value="<?php echo $row['news_image'] ?>" dprocessedid="l7js95">
                 </div>
-                <input class="btn btn-success" style="float: right; margin: 50px 0;" name="add" type="submit" value="Save">
+                <div class="col-12" style="margin-top: 30px;">
+                    <p class="form-label">News Category</p>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="category" id="category" value="<?php echo $row['category'] ?>" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        Debut
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="category" id="category" value="<?php echo $row['category'] ?>" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        Comeback
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="category" id="category" value="<?php echo $row['category'] ?>" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        K-Group
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="category" id="category" value="<?php echo $row['category'] ?>" checked>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        K-Drama
+                      </label>
+                    </div>
+                </div>
+                <input class="btn btn-success" style="float: right; margin: 50px 0;" name="edit" type="submit" value="Save">
             </div>
         </form>
     </div>
---
+++ UAS_Web/edit_news_process.php
@@ -0,0 +1,37 @@
+<?php
+  include("config.php");
+
+$title = $_POST['title'];
+$desc_news = $_POST['desc_news'];
+$content = $_POST['content'];
+$date_news = date("M d Y");
+$news_image = $_FILES['news_image']['name'];
+$category = $_POST['category'];
+
+if (isset($_POST['edit'])) {
+    $ext_allow = array('png','jpg', 'jpeg');
+    $imgnews = $_FILES['news_image']['name'];
+    $x = explode('.', $imgnews);
+    $ext = strtolower(end($x));
+    $size = $_FILES['news_image']['size'];
+    $file_tmp = $_FILES['news_image']['tmp_name'];
+    if(in_array($ext, $ext_allow) === true){
+		if($size < 1044070){
+			move_uploaded_file($file_tmp, './img_news/'.$imgnews);
+            $sql = mysqli_query($conn, "INSERT INTO news (title, desc_news, content, date_news, news_image, category) VALUES ('$title', '$desc_news', '$content', now(), '$imgnews', '$category')");
+					// $query = mysql_query("INSERT INTO upload VALUES(NULL, '$nama')");
+					if($sql){
+						// echo 'FILE BERHASIL DI UPLOAD';
+                        header("location: home.php"); //location output page
+					}else{
+						echo 'Upload file failed';
+					}
+				}else{
+					echo 'File size too big';
+				}
+			}else{
+				echo 'Not allowed uploaded extension file';
+			}
+    header("location:index.php");
+  }
+ ?>
