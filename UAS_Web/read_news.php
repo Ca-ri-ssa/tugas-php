3270b34db31a98f00530f4f7cd74152a908b0ee0
carissa-chandra <carissa.tjan03@gmail.com>
2023-04-10 13:48:30 +0700

    php for today

--- UAS_Web/read_news.php
+++ UAS_Web/read_news.php
@@ -86,7 +86,7 @@
   <div class="container-fluid" style="margin: 80px 0 20px 0; padding: 0 100px; border-bottom: 2px solid #7C924E">
     <h2 style="text-align: center;"><?php echo $row['title'] ?></h2>
     <div style="text-align: right; margin-bottom: 30px;">
-        <a href="edit_news.php?id_news=<?php echo $row['id_news'] ?>" class="stretched-link" name="edit" style="display: inline; margin-left: 800px;">Edit</a>
+        <a href="edit_news.php?id_news=<?php echo $row['id_news'] ?>" class="stretched-link" name="edit" style="display: inline; margin: 0 20px 0 800px;">Edit</a>
         <p style="font-size: 12px; display: inline; margin-right: 20px;">Written by: O-KI</p>
         <p style="font-size: 12px; display: inline;"><?php echo $row['date_news'] ?></p>
     </div>
